643691c419c54f230a4dfa0dc23c77ad67fdeabb
praveenthati <praveen.thati@dish.com>
2017-05-25 12:51:20 -0600

    Add Zuul

--- episodic-shows/src/main/java/com/example/DemoApplication.java
+++ episodic-shows/src/main/java/com/example/DemoApplication.java
@@ -3,7 +3,9 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 @EnableDiscoveryClient
 public class DemoApplication {
